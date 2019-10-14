3db5efb06d618c36f7f889f2830da725e6de32bb
Blieper <yoranmandema@eml.cc>
2019-10-14 13:11:04 +0200

    store handlers on client

--- Orator-Chan/Client/Client.cs
+++ Orator-Chan/Client/Client.cs
@@ -7,5 +7,9 @@ using System.Text;
 namespace OratorChan {
     public class Client : DiscordSocketClient {
         public Configuration Config { get; set; }
+        public GuildData GuildData { get; set; }
+        public CommandHandler CommandHandler { get; set; }
+        public OratorCommandService CommandService { get; set; }
+        public CommunicationHandler CommunicationHandler { get; set; }
     }
 }
